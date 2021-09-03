2b754013d873afb7cf70e84653188a00a50b636a
Thibaud Mathis <thibaud.mathis01@gmail.com>
2021-09-03 17:08:17 +0200

    add navbar corrected

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,6 +11,7 @@ export default function NavBar() {
     const [isScrolled, setIsScrolled] = useState(false)
     window.onscroll = () => {
         setIsScrolled(window.pageYOffset === 0 ? false : true)
+        return() => (window.onscroll = null)
     }
     return (
         <div className={`navbar ${isScrolled ? 'scrolled': ''}`}>
